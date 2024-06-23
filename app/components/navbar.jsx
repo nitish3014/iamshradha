22747540102867a70bb60195db1c84f78a96ad87
Ubuntu <ubuntu@ip-172-31-26-62.ca-central-1.compute.internal>
2024-06-23 14:34:11 +0000

    iamshradha

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT ME</div>
             </Link>
           </li>
           <li>
